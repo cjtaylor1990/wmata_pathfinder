eaf62a33dec6ea79e7996033044acfc1ad3e468e
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-08-08 22:41:20 -0400

    Cleaned up find_metro_path.py. Now is clean and seems to be working.

--- find_metro_path.py
+++ find_metro_path.py
@@ -6,7 +6,12 @@ def find_metro_path(metro, start_station, end_station):
     """
     Takes in a Metro object and the names of the starting and destination stations. Will output an array
     with the stations that are transversed along each line, along with the corresponding line used.
+
+    Currently uses a DFS algorithm and manually puts stations on the current line after the rest, thus
+    ensuring they pop first. This was chosen given the nature of the subway system and wanting to minimize
+    line switching.
     """
+    
     #Checks to see if both the intial and destination stations are in the metro object. If they aren't raise a ValueError
     if start_station not in metro.all_stations.keys():
         raise ValueError("There is not a station named {} in the WMATA system.".format(start_station))
@@ -16,32 +21,29 @@ def find_metro_path(metro, start_station, end_station):
     #Defining a list that will be the main output
     path = [start_station]
 
-    #Setting up distance metric for graph search optimization
-    current_path_length = 0
+    #Creating the stack for the DFS graph search
+    dfs_stack = []
 
-    #Creating the queue for the graph search
-    bs_queue = []
-
-    #Adding in the first stations connections (will need them to do weighting calculations)
+    #Adding in the first stations connections to the stack
     for line in metro.all_stations[start_station].connections.keys():
         for station in metro.all_stations[start_station].connections[line]:
-            bs_queue += [[station, path + [[line, station]]]]
+           dfs_stack += [[station, path + [[line, station]]]]
 
-    #Creating a set that will store the names of the stations visited (to make sure there is no repeats)
-    stations_visited = set()
-    stations_visited.add(start_station)
+    #Creating a set that will store the names of the stations visited (to make sure there is no repeats). Adding starting station to it.
+    stations_visited = set([start_station])
 
-    #While the queue isn't empty
-    while len(bs_queue) != 0:
-        #De-queue the front of the queue 
-        print(bs_queue[-1])
-        current_station, path = bs_queue.pop()
+    #While the stack isn't empty (or until the destination is found)
+    while len(dfs_stack) != 0:
+        
+        #Popping the top entry of stack
+        current_station, path = dfs_stack.pop()
+        
+        #Checking if current station is end station. If it is, returns the path
         if current_station == end_station:
             return path
 
         #Finding current line from last entry in path
         current_line = path[-1][0]
-        print(current_line)
 
         #Finding the corresponding Station object from the Metro object
         current_station_object = metro.all_stations[current_station]
@@ -53,31 +55,15 @@ def find_metro_path(metro, start_station, end_station):
         lines_connected = list(current_station_object.connections.keys())
         lines_connected.remove(current_line)
         lines_connected = lines_connected + [current_line]
-        print(lines_connected)
 
         #For each line that the current station is connected to (i.e. has as a key in its connections dictionary)
-        for line in lines_connected:#current_station_object.connections.keys():
+        for line in lines_connected:
 
-            #if line == current_line:
-            #    current_weight = 0
-            #else:
-            #    current_weight = 1
-
-            #For each station directly connected along the current line
+            #For each connected station in current line
             for station in current_station_object.connections[line]:
 
                 #If that station hasn't been visited (if it has, do nothing)
                 if station not in stations_visited:
-                    """
-                    #If the current station is equal to the station that is being searched for
-                    if station == end_station:
-                        return path + [[line,station]]
-                        #Return the path used to reach the destination
-                        #iff current_path_distance + current_weight < current_min_length:
-                        #    current_min_length = current_path_distance + current_weight
-                        #    current_min_path = path + [[line,station]]
-                    else:
-                        #If it's not the end station, add it to the queue"""
-                    bs_queue += [[station, path + [[line, station]]]]
+                    #Add to the top of the stack
+                    dfs_stack += [[station, path + [[line, station]]]]
 
-    return current_min_path
